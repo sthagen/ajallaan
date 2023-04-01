bcaeec0ed78bba18d89e9cec6ba3380b0f2d9ae2
Stefan Hagen <stefan@hagen.link>
2023-04-01 11:39:51 +0200

    Stamped in the initial version

--- ajallaan/__init__.py
+++ ajallaan/__init__.py
@@ -6,8 +6,8 @@ import pathlib
 from typing import List, no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2023.4.1+parent.eabc8f75'
-# [[[end]]] (checksum: 835a5db409e7f572d77ffb946462f8f6)
+__version__ = '2023.4.1+parent.8d9e9854'
+# [[[end]]] (checksum: 67f1b8d2fa777ead20bfaea4d6e8a152)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
