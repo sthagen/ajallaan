652cdd484fbeeab75375f56d7756f1360d333549
Stefan Hagen <stefan@hagen.link>
2023-04-02 22:09:44 +0200

    Bumped impl version

--- ajallaan/__init__.py
+++ ajallaan/__init__.py
@@ -7,8 +7,8 @@ from enum import Enum
 from typing import List, no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2023.4.1+parent.8d9e9854'
-# [[[end]]] (checksum: 67f1b8d2fa777ead20bfaea4d6e8a152)
+__version__ = '2023.4.2+parent.2637a057'
+# [[[end]]] (checksum: 203623ce1610b41c9139f2b591ffc9ff)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
